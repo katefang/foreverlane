a11212adcd6b72edc46e54bbd8952a8ad5ccc92f
Kate Fang <Kate@MacBook-Pro.local>
2020-06-13 22:38:27 -0400

    delete react logos

--- src/App.js
+++ src/App.js
@@ -1,32 +1,18 @@
-<<<<<<< HEAD
 import React from "react";
-import logo from "./logos/logo.svg";
+import { Route } from "react-router-dom";
+
 import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <h1>Hello Peter!</h1>
-    </div>
-  );
-}
-=======
-import React from 'react';
-import { Route } from 'react-router-dom';
-
-import './App.css';
-
-import Header from './components/header/header.component';
-import Navbar from './components/nav-bar/nav-bar.component';
-import Homepage from './pages/homepage/homepage.component';
+import Header from "./components/header/header.component";
+import Navbar from "./components/nav-bar/nav-bar.component";
+import Homepage from "./pages/homepage/homepage.component";
 
 const App = () => (
   <div>
     <Header />
     <Navbar />
-    <Route path='/' component={Homepage} />
+    <Route path="/" component={Homepage} />
   </div>
 );
->>>>>>> c26f94b695d1d76f4567d1d1074d9268d0d01483
 
 export default App;
