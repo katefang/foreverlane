0f06a5fba8d6a576072e85cb6dc9bf1251e6e048
Peter Cho <pcho51990@gmail.com>
2020-06-13 22:41:17 -0400

    formatting

--- src/App.js
+++ src/App.js
@@ -1,22 +1,17 @@
-<<<<<<< HEAD
 import React from 'react';
 import { Route } from 'react-router-dom';
-=======
-import React from "react";
-import { Route } from "react-router-dom";
->>>>>>> a11212adcd6b72edc46e54bbd8952a8ad5ccc92f
 
-import "./App.css";
+import './App.css';
 
-import Header from "./components/header/header.component";
-import Navbar from "./components/nav-bar/nav-bar.component";
-import Homepage from "./pages/homepage/homepage.component";
+import Header from './components/header/header.component';
+import Navbar from './components/nav-bar/nav-bar.component';
+import Homepage from './pages/homepage/homepage.component';
 
 const App = () => (
   <div>
     <Header />
     <Navbar />
-    <Route path="/" component={Homepage} />
+    <Route path='/' component={Homepage} />
   </div>
 );
 
