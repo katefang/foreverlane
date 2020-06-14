7b4be7e090d57e3800ed9c277b94de95c2b71342
Kate Fang <Kate@MacBook-Pro.local>
2020-06-13 20:04:38 -0400

    change app.js

--- src/App.js
+++ src/App.js
@@ -3,7 +3,11 @@ import logo from "./logo.svg";
 import "./App.css";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <h1>Hello Peter!</h1>
+    </div>
+  );
 }
 
 export default App;
